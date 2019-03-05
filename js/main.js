c38f59884388cd42411df3efc1b0b647c798f713
oyiooi <15055142531@163.com>
2019-03-05 09:33:32 +0800

    注册serviceWorker‘

--- js/main.js
+++ js/main.js
@@ -209,3 +209,21 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
+//register service worker
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+    
+    if (reg.installing) {
+      console.log('Service worker installing');
+    } else if (reg.waiting) {
+      console.log('Service worker installed');
+    } else if (reg.active){
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error){
+    console.log('Registration failed with'+error);
+  });
+}
+
