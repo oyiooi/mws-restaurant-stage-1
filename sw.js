e76670db37a6851920b8ee2516dee27bee532e85
oyiooi <15055142531@163.com>
2019-03-05 09:38:57 +0800

    将站点所有资源添加到caches；hijack 请求并用缓存资源回复

---
+++ sw.js
@@ -0,0 +1,37 @@
+//install cache
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open('ver01').then(function(cache) {
+            return cache.addAll([
+                '/',
+                'data/restaurants.json',
+                'index.html',
+                'css/styles.css',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'restaurant.html'
+            ]);
+        })
+    );
+});
+
+//hijacking request
+self.addEventListener('fetch',function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response) return response;
+            return fetch(event.request);
+        })
+    );
+});
